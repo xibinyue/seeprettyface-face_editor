19a8a150b47fe61d933b97501ff3e3b52a00ab48
xibin.yue <xibin.yue@moji.com>
2019-10-22 11:00:47 +0800

    rpc

--- gan_server.py
+++ gan_server.py
@@ -10,6 +10,7 @@ from util.generator_model import Generator
 import os
 from util.utils import *
 import json
+import tensorflow as tf
 
 from play_with_dlatent import read_feature, generate_image
 
@@ -17,6 +18,7 @@ from play_with_dlatent import read_feature, generate_image
 class ImageGenerator(object):
     def __init__(self):
         tflib.init_tf()
+        self.g1 = tf.Graph()
 
     def move_latent_and_save(self, latent_vector, direction, coeffs, generator):
         res = []
@@ -30,18 +32,19 @@ class ImageGenerator(object):
         return res
 
     def generate(self, model, latent, coeffs):
-        with open(os.path.join('model', models[model]), "rb") as f:
-            generator_network, discriminator_network, Gs_network = pickle.load(f)
-        generator = Generator(Gs_network, batch_size=1, randomize_noise=False)
+        with self.g1.as_default():
+            with open(os.path.join('model', models[model]), "rb") as f:
+                generator_network, discriminator_network, Gs_network = pickle.load(f)
+            generator = Generator(Gs_network, batch_size=1, randomize_noise=False)
 
-        face_latent = read_feature('input_latent/0001.txt')
-        stack_latents = np.stack(face_latent for _ in range(1))
-        face_dlatent = Gs_network.components.mapping.run(stack_latents, None)
-        direction = np.load(latents[latent])
-        # coeffs = [-5., -4., -3., -2., -1., 0., 1., 2., 3., 4.]
-        imgs_list = self.move_latent_and_save(face_dlatent, direction, coeffs, generator)
-        del generator, face_dlatent, face_latent
-        return json.dumps({'imgs': imgs_list})
+            face_latent = read_feature('input_latent/0001.txt')
+            stack_latents = np.stack(face_latent for _ in range(1))
+            face_dlatent = Gs_network.components.mapping.run(stack_latents, None)
+            direction = np.load(latents[latent])
+            # coeffs = [-5., -4., -3., -2., -1., 0., 1., 2., 3., 4.]
+            imgs_list = self.move_latent_and_save(face_dlatent, direction, coeffs, generator)
+            del generator, face_dlatent, face_latent
+            return json.dumps({'imgs': imgs_list})
 
 
 def server_gan():
