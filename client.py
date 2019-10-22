7fa559fd1cd0a35bb103e034d03aa18a98f838e8
xibin.yue <xibin.yue@moji.com>
2019-10-22 09:49:04 +0800

    rpc

--- client.py
+++ client.py
@@ -3,6 +3,6 @@
 # Copyright (c) 2018 - xibin.yue <xibin.yue@moji.com>
 import zerorpc
 
-c = zerorpc.Client()
+c = zerorpc.Client(heartbeat=None)
 c.connect("tcp://0.0.0.0:8688")
 print(c.generate('黄种人', '微笑程度', [-1., 0., 1.]))
